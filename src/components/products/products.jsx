adae6ff0fbc762ba40f1b6cd75ed0227c8b1ef21
Cristobal Licea <cris.licea@gmail.com>
2022-07-12 17:24:56 -0500

    more Iphone changes

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import Card from "../card/card";
 import './products.css'
 import Nav from '../nav/nav'
 
 const Products = () => {
   return (
     <div id="productsPage">
-      <h1>Products</h1>
       <Nav />
-      <div className="products" style={{ display: 'flex', flexWrap: 'wrap'}}>
-      <Card title="Granite Countertops"/>
-      <Card title="Marble Countertops"/>
-      <Card title="Accessories"/>
-      <Card title="Faucets"/>
-      <Card title="Maintenece"/>
-      <Card title="Granite Countertops"/>
+      <div className="productsHeader">
+        <h1>Products</h1>
       </div>
       
 
