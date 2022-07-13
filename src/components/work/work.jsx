944964b612476a04b463a6c13ce49d45e21cb247
Cristobal Licea <cris.licea@gmail.com>
2022-07-12 20:46:17 -0500

    added and designed work page

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import Nav from '../nav/nav'
+import './work.css'
+import Card from '../card/card'
+import { useNavigate } from "react-router-dom";
 
 const Work = () => {
+  let navigate = useNavigate();
   return (
     <div>
       <Nav />
+      <div className="workPage">
+      <div className="worksHeader">
+        <h1>Our Work</h1>
+      </div>
+      <div className="works">
+
+        <div className="work" onClick={() => {navigate('/products/granite')}}>
+          <Card title="Testimonials" />
+        </div>
+        <div className="work" onClick={() => {navigate('/products/marble')}}>
+          <Card title="Leave a Review"/>
+        </div>
+        <div className="work" onClick={() => {navigate('/products/quartz')}}>
+          <Card title="Gallery"/>
+        </div>
+      </div>
+        
+        
+        
+      </div>
     </div>
   )
 }
