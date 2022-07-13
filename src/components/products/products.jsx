1870c877e8687b2763016210f45bc5cb858b2d1f
Cristobal Licea <cris.licea@gmail.com>
2022-07-12 19:53:31 -0500

    made navigation changes as well as add pages

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,21 +1,46 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './products.css'
 import Nav from '../nav/nav'
 import Card from '../card/card'
 
 const Products = () => {
+  let navigate = useNavigate();
+
+  const navi = (e) => {
+    navigate('/' + e.target.id);
+  }
+
   return (
     <div id="productsPage">
       <Nav />
       <div className="productsHeader">
-        <h1>Products</h1>
+        <h1 onClick={navi} id='products'>Products</h1>
       </div>
       <div className="products">
-        <Card title="Granite"/>
-        <Card title="Marble"/>
-        <Card title="Quartz"/>
-        <Card title="Faucets"/>
-        <Card title="Sinks"/>
+
+        <div className="product" onClick={() => {navigate('/products/granite')}}>
+          <Card title="Granite" />
+        </div>
+        <div className="product" onClick={() => {navigate('/products/marble')}}>
+          <Card title="Marble"/>
+        </div>
+        <div className="product" onClick={() => {navigate('/products/quartz')}}>
+          <Card title="Quartz"/>
+        </div>
+        <div className="product" onClick={() => {navigate('/products/Sinks')}}>
+          <Card title="Sinks"/>
+        </div>
+        <div className="product" onClick={() => {navigate('/products/Faucets')}}>
+          <Card title="Faucets"/>
+        </div>
+        
+          
+        
+       
+        
+        
+        
       </div>
 
     </div>
