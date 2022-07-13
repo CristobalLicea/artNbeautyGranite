a2850451d173132abd626a37a69726471ee55814
Cristobal Licea <cris.licea@gmail.com>
2022-07-13 17:58:08 -0500

    .

--- src/components/mobile/imgCard.jsx
+++ src/components/mobile/imgCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
 import { useState } from "react";
 import { INTRO } from "../../constants";
 import './imgCard.css'
@@ -7,41 +6,25 @@ import './imgCard.css'
 const ImgCard = () => {
   let i = 0
   const [image, setImage] = useState(INTRO[i])
-  
-
-  const tick = () => {
-    console.log('ticked')
-    if (i === INTRO.length - 1) {
-      console.log(i)
-    } else {
-      console.log(i)
-    } 
-  }
+  const [num, setNum] = useState(0)
 
   const hello = () => {
-    console.log('1')
-    console.log(INTRO.length)
-    if (i === INTRO.length - 1) {
-      console.log(i)
-      i = 0
+    if (num === INTRO.length - 1) {
+      setNum(0)
     } else {
-      console.log(i)
-      i++;
+      setNum(num + 1)
     }
-    setImage(INTRO[i])
   }
 
-  const timer = setInterval(hello, 8000)
-  
-    
+
   return (
     
     <div className="imgCard">
-      <img src={image} alt="" />
+      <img src={INTRO[num]} alt="" />
       <div className="imgCardOverlay">
         <h2>Redesign your home with Art & Beauty</h2>
         <p>We offer the the best prices in San Antonio! Let us help you reinvent your Kitchen, Restroom, Office, or Any Countertop that needs a new look!</p>
-        <button>BROWSE OUR INVENTORY</button>
+        <button onClick={hello}>BROWSE OUR INVENTORY</button>
         <button>VIEW OUR GALLERY</button>
       </div>
     </div>
